627e3fb648854ec04d359efe51f3f8dd5f2df59a
xz65669983 <zzcfujI@126.com>
2017-06-10 21:54:06 +0800

    第二个页面制作完成

--- app/src/main/java/com/example/administrator/littletortoisetortoise/ActivityActivity.java
+++ app/src/main/java/com/example/administrator/littletortoisetortoise/ActivityActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ListView;
@@ -18,6 +19,7 @@ public class ActivityActivity extends AppCompatActivity {
 
     private Toolbar tb;
     private ListView lv_activity_center;
+    private View list_header;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,7 +32,9 @@ public class ActivityActivity extends AppCompatActivity {
     }
 
     private void initiallistview() {
+        lv_activity_center.addHeaderView(list_header);
         lv_activity_center.setAdapter(new ActivityCenterAdapter());
+
     }
 
     private void initialtoolbar() {
@@ -43,6 +47,7 @@ public class ActivityActivity extends AppCompatActivity {
     private void findId() {
         tb = (Toolbar) findViewById(R.id.tb);
         lv_activity_center = (ListView) findViewById(R.id.lv_activity_center);
+        list_header = LayoutInflater.from(this).inflate(R.layout.head_lv_activitiy_center, null);
     }
 
     @Override
