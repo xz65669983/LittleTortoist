d47b3dda9a625d0e36b0bf64e910e600a232f709
xz65669983 <zzcfujI@126.com>
2017-06-18 08:22:29 +0800

    首页动画制作

--- app/src/main/java/com/example/administrator/littletortoisetortoise/EventDetailsActivity.java
+++ app/src/main/java/com/example/administrator/littletortoisetortoise/EventDetailsActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.littletortoisetortoise;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -17,13 +18,13 @@ public class EventDetailsActivity extends AppCompatActivity {
     private Toolbar tb;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eventdetail);
         findId();
         initialtoolbar();
-
     }
+
     private void initialtoolbar() {
         tb.setTitle("");
         setSupportActionBar(tb);
---
+++ app/src/main/java/com/example/administrator/littletortoisetortoise/SplashActivity.java
@@ -0,0 +1,94 @@
+package com.example.administrator.littletortoisetortoise;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Administrator on 2017/6/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    private LinearLayout ll_splash;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_splash);
+        ll_splash = (LinearLayout) findViewById(R.id.ll_splash);
+        startAnim();
+    }
+    /**
+     * 开启动画
+     */
+    private void startAnim() {
+
+        // 动画集合
+        AnimationSet set = new AnimationSet(false);
+
+        // 旋转动画
+        RotateAnimation rotate = new RotateAnimation(0, 360,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        rotate.setDuration(1000);// 动画时间
+        rotate.setFillAfter(true);// 保持动画状态
+
+        // 缩放动画
+        ScaleAnimation scale = new ScaleAnimation(0, 1, 0, 1,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        scale.setDuration(1000);// 动画时间
+        scale.setFillAfter(true);// 保持动画状态
+
+        // 渐变动画
+        AlphaAnimation alpha = new AlphaAnimation(0, 1);
+        alpha.setDuration(2000);// 动画时间
+        alpha.setFillAfter(true);// 保持动画状态
+
+        set.addAnimation(rotate);
+        set.addAnimation(scale);
+        set.addAnimation(alpha);
+
+        // 设置动画监听
+        set.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+
+            // 动画执行结束
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                jumpNextPage();
+            }
+        });
+
+        ll_splash.startAnimation(set);
+    }
+
+    /**
+     * 跳转下一个页面
+     */
+    private void jumpNextPage() {
+
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+
+        finish();
+    }
+
+}
