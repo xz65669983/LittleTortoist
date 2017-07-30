ef237367f2c166e63e596fa762ea9b9ac7cd207b
xz65669983 <zzcfujI@126.com>
2017-07-30 11:24:20 +0800

    0730 11:24

---
+++ app/src/main/java/com/example/administrator/littletortoisetortoise/personalInformationActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.littletortoisetortoise;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/7/30.
+ */
+
+public class personalInformationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_personal_informantion);
+    }
+}
